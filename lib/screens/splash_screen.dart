71dfdcfcf3836e5797cf486dc9291c4cb5e54433
Ndraa234 <andragio87@gmail.com>
2025-05-12 15:42:28 +0700

    aloha

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,7 @@
 // lib/screens/splash_screen.dart
 
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacementNamed(context, '/login');
     });
   }
@@ -22,25 +22,11 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xF0FAFFF8),
+      backgroundColor: const Color(0xFF004D40),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logosplash.png', // ganti dengan path logo kamu
-              height: 100,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'easier',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF0D4D4D),
-              ),
-            ),
-          ],
+        child: Image.asset(
+          'assets/images/splash.png',
+          width: 200,
         ),
       ),
     );
