a3e4c7d98ca72ac4536558d8d7d757864672a512
Ndraa234 <andragio87@gmail.com>
2025-05-14 09:00:38 +0700

    itu  aja

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,12 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/profil/bindings/profil_binding.dart';
+import '../modules/profil/views/profil_view.dart';
+import '../modules/register/bindings/register_binding.dart';
+import '../modules/register/views/register_view.dart';
+import '../modules/tugas/bindings/tugas_binding.dart';
+import '../modules/tugas/views/tugas_view.dart';
 import '../widgets/splash_screen.dart';
 
 part 'app_routes.dart';
@@ -27,6 +33,21 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => const LoginScreen(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.TUGAS,
+      page: () => const TugasView(),
+      binding: TugasBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => const RegisterView(),
+      binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFIL,
+      page: () => const ProfilView(),
+      binding: ProfilBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,6 +6,9 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const SPLASH = _Paths.SPLASH;
   static const LOGIN = _Paths.LOGIN;
+  static const TUGAS = _Paths.TUGAS;
+  static const REGISTER = _Paths.REGISTER;
+  static const PROFIL = _Paths.PROFIL;
 }
 
 abstract class _Paths {
@@ -13,4 +16,7 @@ abstract class _Paths {
   static const HOME = '/home';
   static const SPLASH = '/splash';
   static const LOGIN = '/login';
+  static const TUGAS = '/tugas';
+  static const REGISTER = '/register';
+  static const PROFIL = '/profil';
 }
